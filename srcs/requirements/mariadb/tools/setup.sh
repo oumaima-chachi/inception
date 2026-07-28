109c7c0aff15ab563ec3b95278cfb2420338b381
oumaima-chachi <159659102+oumaima-chachi@users.noreply.github.com>
2026-07-28 15:20:18 +0100

    feat(mariadb): add condition to check if target database is already initialized
    Guard initialization block with directory existence check.

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,3 +3,7 @@ set -e
 
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+    echo "Initializing MariaDB..."
+fi
