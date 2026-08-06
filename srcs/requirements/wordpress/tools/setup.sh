3ca2fd154fc5cb5dd10eced0a27bad54e738ea31
oumaima-chachi <159659102+oumaima-chachi@users.noreply.github.com>
2026-08-06 11:14:25 +0100

    feat(wordpress): implement database readiness polling loop using mysqladmin
    Add loop polling MariaDB connection until healthy.

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,3 +4,7 @@ set -e
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 WP_ADMIN_PASSWORD=$(grep WP_ADMIN_PASSWORD /run/secrets/credentials | cut -d '=' -f2-)
 WP_USER_PASSWORD=$(grep WP_USER_PASSWORD /run/secrets/credentials | cut -d '=' -f2-)
+
+while ! mysqladmin ping -h mariadb -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent; do
+    sleep 1
+done
