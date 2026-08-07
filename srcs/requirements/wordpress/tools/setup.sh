1bec422e7927a5201940de2892886594d7376194
oumaima-chachi <159659102+oumaima-chachi@users.noreply.github.com>
2026-08-07 09:35:12 +0100

    feat(wordpress): execute automated WordPress installation via WP-CLI
    Add wp core install with DOMAIN_NAME, admin credentials and title.

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -21,4 +21,12 @@ if [ ! -f wp-config.php ]; then
         --dbpass="${MYSQL_PASSWORD}" \
         --dbhost=mariadb:3306 \
         --allow-root
+
+    wp core install \
+        --url="${DOMAIN_NAME}" \
+        --title="Inception" \
+        --admin_user="${WP_ADMIN_USER}" \
+        --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" \
+        --allow-root
 fi
